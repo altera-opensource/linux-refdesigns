656498197e7b4f5340483ad62f2ce018767178ff
Ong, Hean Loong <hean.loong.ong@intel.com>
2020-02-18 21:09:29 +0800

    use snprintf instead of snprintf_s_i
    Signed-off-by: Ong, Hean Loong <hean.loong.ong@intel.com>

--- blink/blink.c
+++ blink/blink.c
@@ -32,7 +32,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#include "../include/snprintf_s.h"
 #include "led_control.h"
 
 #define STRSIZE 256
@@ -42,8 +41,8 @@ int main(int argc, char** argv)
 	FILE *fp;
 	int led, delay_ms, i;
 	char trigger[] = "timer";
-	char dir[100];
-	char delay[10];
+	char dir[STRSIZE];
+	char delay[STRSIZE];
 	int blink_interval_ms;
 
 	for (i = 1; i < argc; i++) {
@@ -75,8 +74,8 @@ int main(int argc, char** argv)
 	/* Checking if scrolling is still happening
 	   if it is, we will inform user, and bail
 	*/
-	char get_fifo_scroll[2], seconds_bet_toggle[10];
-	snprintf_s_i(get_fifo_scroll, STRSIZE-1, "0", 0);
+	char get_fifo_scroll[STRSIZE], seconds_bet_toggle[10];
+	snprintf(get_fifo_scroll, STRSIZE-1, "0");
 	fp = fopen("/home/root/.intelFPGA/frequency_fifo_scroll", "w");
 	if (fp == NULL) {
 		printf("Failed opening fifo frequency_fifo_scroll\n");
@@ -110,12 +109,12 @@ int main(int argc, char** argv)
 	/* Setting the trigger to blinking */
 	setLEDtrigger(led, trigger, sizeof(trigger));
 	/* Setting the delay */
-	snprintf_s_i(dir, STRSIZE-1, "/sys/class/leds/fpga_led%d/delay_on", led);
+	snprintf(dir, STRSIZE-1, "/sys/class/leds/fpga_led%d/delay_on", led);
 	if ((fp = fopen(dir, "w")) == NULL) {
 		printf("Failed to open the file %s\n" , dir);
 	}
 	else {
-		snprintf_s_i(delay, STRSIZE-1,"%d", blink_interval_ms);
+		snprintf(delay, STRSIZE-1,"%d", blink_interval_ms);
 		if (fwrite(delay, 1, sizeof(delay), fp) == 0)
 		{
 			fclose(fp);
@@ -123,12 +122,12 @@ int main(int argc, char** argv)
 		}
 		fclose(fp);
 	}
-	snprintf_s_i(dir, STRSIZE-1,"/sys/class/leds/fpga_led%d/delay_off", led);
+	snprintf(dir, STRSIZE-1,"/sys/class/leds/fpga_led%d/delay_off", led);
 	if ((fp = fopen(dir, "w")) == NULL) {
 		printf("Failed to open the file %s\n" , dir);
 	}
 	else {
-		snprintf_s_i(delay, STRSIZE-1,"%d", blink_interval_ms);
+		snprintf(delay, STRSIZE-1,"%d", blink_interval_ms);
                 if (fwrite(delay, 1, sizeof(delay), fp) == 0)
                 {
                         fclose(fp);
